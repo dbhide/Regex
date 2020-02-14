037f03d0b798e7dd6d8191cc14d2f3b11e1119dc
dbhide <dhanashree.bhide3@gmail.com>
2020-02-14 12:10:14 +0530

    ADDED: Email Validation

--- UserResgistration.sh
+++ UserResgistration.sh
@@ -3,14 +3,25 @@ shopt -s extglob
 
 echo "Welcome to User Registration Problem"
 
+#To validate all details entered by user
+function validateUserDetails() {
+	userDetails=$1
+	validDetails=$2
+	if [[ $userDetails =~ $validDetails ]]
+	then
+		echo $userDetails
+	else
+		echo "Invalid Entry!!"
+	fi
+}
+
 read -p "Enter First Name and Last Name : " firstName lastName
+validateUserDetails $firstName $nameRule
+validateUserDetails $lastName $nameRule
+
+read -p "Enter Email Id : " email
+validateUserDetails $email $emailRule
 
 nameRule="^([A-Z]{1}[a-z]*)$"
 
-if [[ $firstName =~ $nameRule && $lastName =~ $nameRule ]]
-then
-	echo "First Name: " $firstName
-	echo "Last Name: " $lastName
-else
-	echo "Invalid entry..Enter valid Name"
-fi
+emailRule="^([a-zA-Z]{3,}|[a-zA-Z0-9]{3,}[._+-]{1}[a-zA-Z0-9]{3,})[@]{1}[a-zA-Z0-9]*[.]{1}[a-z]{2,3}|[.]{1}[a-z]{2,3}$"
