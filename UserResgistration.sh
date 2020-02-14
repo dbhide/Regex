a6ac794df692378746f22da84f61e669f57e6395
dbhide <dhanashree.bhide3@gmail.com>
2020-02-14 11:01:20 +0530

    ADDED: Validation for First Name

--- UserResgistration.sh
+++ UserResgistration.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
+shopt -s extglob
 
 echo "Welcome to User Registration Problem"
+
+read -p "Enter First Name : " firstName
+
+firstNameRule="^([A-Z]{1}[a-z]*)$"
+
+if [[ $firstName =~ $firstNameRule ]]
+then
+	echo "First Name: " $firstName
+else
+	echo "Invalid entry..Enter valid Name"
+fi
