ef822b9a4648d9605777a3c31fa103cd81fdd9dd
dbhide <dhanashree.bhide3@gmail.com>
2020-02-14 15:12:52 +0530

    ADDED: Function to validate user mobile number

--- UserResgistration.sh
+++ UserResgistration.sh
@@ -7,12 +7,21 @@ echo "Welcome to User Registration Problem"
 function validateUserDetails() {
 	userDetails=$1
 	validDetails=$2
-	if [[ $userDetails =~ $validDetails ]]
+	if [[ $1 =~ $validDetails ]]
 	then
 		echo $userDetails
 	else
 		echo "Invalid Entry!!"
 	fi
+}
+
+function validateMobileNumber() {
+   if [[ $mobileNumber =~ $mobileNumberRule ]]
+   then
+      echo $mobileNumber
+   else
+      echo "Invalid"
+   fi
 }
 
 read -p "Enter First Name and Last Name : " firstName lastName
@@ -22,6 +31,9 @@ validateUserDetails $lastName $nameRule
 read -p "Enter Email Id : " email
 validateUserDetails $email $emailRule
 
-nameRule="^([A-Z]{1}[a-z]*)$"
+read -p "Enter Mobile Number : " mobileNumber
+validateMobileNumber
 
+nameRule="^([A-Z]{1}[a-z]*)$"
 emailRule="^([a-zA-Z]{3,}|[a-zA-Z0-9]{3,}[._+-]{1}[a-zA-Z0-9]{3,})[@]{1}[a-zA-Z0-9]*[.]{1}[a-z]{2,3}|[.]{1}[a-z]{2,3}$"
+mobileNumberRule="^([0-9]{2}\s{1}[0-9]{10})$"
