c4ce591aa37790606cfbd14eeb7e716a9ac0bdf7
dbhide <dhanashree.bhide3@gmail.com>
2020-02-14 16:29:13 +0530

    ADDED: Password Validation for minimum 8 characters

--- UserResgistration.sh
+++ UserResgistration.sh
@@ -1,27 +1,22 @@
-#!/bin/bash -x
+#!/bin/bash -x 
 shopt -s extglob
 
 echo "Welcome to User Registration Problem"
+nameRule="^[A-Z]{1}[a-z]{2,}$"
+emailRule="^([a-zA-Z]{3,}|[a-zA-Z0-9]{3,}[._+-]{1}[a-zA-Z0-9]{3,})[@]{1}[a-zA-Z0-9]*[.]{1}[a-z]{2,3}|[.]{1}[a-z]{2,3}$"
+mobileNumberRule="^([0-9]{2}\s{1}[0-9]{10})$"
+passwordRule="^[a-zA-Z]{8,}$"
 
 #To validate all details entered by user
 function validateUserDetails() {
 	userDetails=$1
 	validDetails=$2
-	if [[ $1 =~ $validDetails ]]
+	if [[ $userDetails =~ $validDetails ]]
 	then
 		echo $userDetails
 	else
 		echo "Invalid Entry!!"
 	fi
-}
-
-function validateMobileNumber() {
-   if [[ $mobileNumber =~ $mobileNumberRule ]]
-   then
-      echo $mobileNumber
-   else
-      echo "Invalid"
-   fi
 }
 
 read -p "Enter First Name and Last Name : " firstName lastName
@@ -32,8 +27,7 @@ read -p "Enter Email Id : " email
 validateUserDetails $email $emailRule
 
 read -p "Enter Mobile Number : " mobileNumber
-validateMobileNumber
+validateUserDetails "$mobileNumber" $mobileNumberRule
 
-nameRule="^([A-Z]{1}[a-z]*)$"
-emailRule="^([a-zA-Z]{3,}|[a-zA-Z0-9]{3,}[._+-]{1}[a-zA-Z0-9]{3,})[@]{1}[a-zA-Z0-9]*[.]{1}[a-z]{2,3}|[.]{1}[a-z]{2,3}$"
-mobileNumberRule="^([0-9]{2}\s{1}[0-9]{10})$"
+read -p  "Enter Password : " password
+validateUserDetails $password $passwordRule
